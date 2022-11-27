64d3ee94c7181a830016df3e6196e6bcf254237c
Carlos Delgado <cardel87@gmail.com>
2022-11-27 11:17:59 -0500

    Fixing testing cases adding the father reference

--- app/src/test/java/rojinegros/data/ArbolRojiNegroGenerador.java
+++ app/src/test/java/rojinegros/data/ArbolRojiNegroGenerador.java
@@ -3,139 +3,158 @@ package rojinegros.data;
 import rojinegros.ArbolRojinegro;
 
 public class ArbolRojiNegroGenerador {
-    /*
-     * Ejemplo1 para rotación a la izquierda.
-     */
-    public ArbolRojinegro ejemplo1() {
-        ArbolRojinegro ejemplo = new ArbolRojinegro(
-                new ArbolRojinegro(
-                        null,
-                        null,
-                        1,
-                        true
-                ),
-                new ArbolRojinegro(
-                        new ArbolRojinegro(
+        /*
+         * Ejemplo1 para rotación a la izquierda.
+         */
+        public ArbolRojinegro ejemplo1() {
+                ArbolRojinegro nodoIzq = new ArbolRojinegro(
+                                null,
+                                null,
+                                1,
+                                true);
+
+                ArbolRojinegro nodoDerIzq = new ArbolRojinegro(
                                 null,
                                 null,
                                 3,
-                                true
-                        ),
-                        new ArbolRojinegro(
+                                true);
+                ArbolRojinegro nodoDerDer = new ArbolRojinegro(
                                 null,
                                 null,
                                 5,
-                                true
-                        ),
-                        4,
-                        false
-                ),
-                2,
-                true
-        );
-        return ejemplo;
-    }
+                                true);
+                ArbolRojinegro nodoDer = new ArbolRojinegro(
+                                nodoDerIzq,
+                                nodoDerDer,
+                                4,
+                                false);
+                ArbolRojinegro raiz = new ArbolRojinegro(
+                                nodoIzq,
+                                nodoDer,
+                                2,
+                                true);
 
-    /*
-     * Ejemplo2 para rotación a la izquierda.
-     */
-    public ArbolRojinegro ejemplo2() {
-        ArbolRojinegro ejemplo = new ArbolRojinegro(
-                new ArbolRojinegro(
-                        null,
-                        null,
-                        1,
-                        true
-                ),
-                new ArbolRojinegro(
-                        new ArbolRojinegro(
+                nodoDer.setFather(raiz);
+                nodoIzq.setFather(raiz);
+                nodoDerIzq.setFather(nodoDer);
+                nodoDerDer.setFather(nodoDer);
+                return raiz;
+        }
+
+        /*
+         * Ejemplo2 para rotación a la izquierda.
+         */
+        public ArbolRojinegro ejemplo2() {
+                ArbolRojinegro nodoIzq = new ArbolRojinegro(
+                                null,
+                                null,
+                                1,
+                                true);
+
+                ArbolRojinegro nodoDerIzq = new ArbolRojinegro(
                                 null,
                                 null,
                                 6,
-                                true
-                        ),
-                        new ArbolRojinegro(
+                                true);
+
+                ArbolRojinegro nodoDerDer = new ArbolRojinegro(
                                 null,
                                 null,
                                 9,
-                                true
-                        ),
-                        8,
-                        false
-                ),
-                5,
-                true
-        );
-        return ejemplo;
-    }
+                                true);
 
+                ArbolRojinegro nodoDer = new ArbolRojinegro(
+                                nodoDerIzq,
+                                nodoDerDer,
+                                8,
+                                false);
+                ArbolRojinegro raiz = new ArbolRojinegro(
+                                nodoIzq,
+                                nodoDer,
+                                5,
+                                true);
+                nodoDer.setFather(raiz);
+                nodoIzq.setFather(raiz);
+                nodoDerIzq.setFather(nodoDer);
+                nodoDerDer.setFather(nodoDer);
+                return raiz;
+        }
 
-    /*
-     * Ejemplo3 para rotación a la derecha.
-     */
-    public ArbolRojinegro ejemplo3() {
-        ArbolRojinegro ejemplo = new ArbolRojinegro(
-
-                new ArbolRojinegro(
-                        new ArbolRojinegro(
+        /*
+         * Ejemplo3 para rotación a la derecha.
+         */
+        public ArbolRojinegro ejemplo3() {
+                ArbolRojinegro nodoIzqIzq = new ArbolRojinegro(
                                 null,
                                 null,
                                 1,
-                                true
-                        ),
-                        new ArbolRojinegro(
+                                true);
+
+                ArbolRojinegro nodoIzqDer = new ArbolRojinegro(
                                 null,
                                 null,
                                 3,
-                                true
-                        ),
-                        2,
-                        false
-                ),
-                new ArbolRojinegro(
-                        null,
-                        null,
-                        5,
-                        true
-                ),
-                4,
-                true
-        );
-        return ejemplo;
-    }
+                                true);
 
+                ArbolRojinegro nodoIzq = new ArbolRojinegro(
+                                nodoIzqIzq,
+                                nodoIzqDer,
+                                2,
+                                false);
 
-    /*
-     * Ejemplo4 para rotación a la derecha.
-     */
-    public ArbolRojinegro ejemplo4() {
-        ArbolRojinegro ejemplo = new ArbolRojinegro(
+                ArbolRojinegro nodoDer = new ArbolRojinegro(
+                                null,
+                                null,
+                                5,
+                                true);
+                ArbolRojinegro raiz = new ArbolRojinegro(
+                                nodoIzq,
+                                nodoDer,
+                                4,
+                                true);
 
-                new ArbolRojinegro(
-                        new ArbolRojinegro(
+                nodoIzq.setFather(raiz);
+                nodoDer.setFather(raiz);
+                nodoIzqDer.setFather(nodoDer);
+                nodoIzqIzq.setFather(nodoIzq);
+                return raiz;
+        }
+
+        /*
+         * Ejemplo4 para rotación a la derecha.
+         */
+        public ArbolRojinegro ejemplo4() {
+                ArbolRojinegro nodoIzqIzq = new ArbolRojinegro(
                                 null,
                                 null,
                                 1,
-                                true
-                        ),
-                        new ArbolRojinegro(
+                                true);
+                ArbolRojinegro nodoIzqDer = new ArbolRojinegro(
                                 null,
                                 null,
                                 6,
-                                true
-                        ),
-                        5,
-                        false
-                ),
-                new ArbolRojinegro(
-                        null,
-                        null,
-                        9,
-                        true
-                ),
-                8,
-                true
-        );
-        return ejemplo;
-    }
+                                true);
+
+                ArbolRojinegro nodoIzq = new ArbolRojinegro(
+                                nodoIzqIzq,
+                                nodoIzqDer,
+                                5,
+                                false);
+                ArbolRojinegro nodoDer = new ArbolRojinegro(
+                                null,
+                                null,
+                                9,
+                                true);
+                ArbolRojinegro raiz = new ArbolRojinegro(
+                                nodoIzq,
+                                nodoDer,
+                                8,
+                                true);
+
+                nodoIzq.setFather(raiz);
+                nodoDer.setFather(raiz);
+                nodoIzqDer.setFather(nodoDer);
+                nodoIzqIzq.setFather(nodoIzq);
+                return raiz;
+        }
 }
